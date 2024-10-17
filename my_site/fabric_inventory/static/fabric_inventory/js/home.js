9bcd958c6a48957e9d5aab5e9d078ba4412fa6cb
vilgefortz <ckoshkin@yandex.ru>
2024-10-17 23:19:18 +0300

    Пока не работает модальные для удалить

--- my_site/fabric_inventory/static/fabric_inventory/js/home.js
+++ my_site/fabric_inventory/static/fabric_inventory/js/home.js
@@ -1,11 +1,29 @@
 const delete_modal = new bootstrap.Modal(document.getElementById('deleteModal'));
-const delete_btn = document.getElementById('confirmDelete');
-
-delete_modal.addEventListener('click', function() {
+const delete_btn = document.getElementById('delete');
+console.log(delete_modal);
+console.log(delete_btn);
+delete_btn.addEventListener('click', function() {
     delete_modal.hide(); // Закрываем модальное окно
 
 });
 
+document.getElementById('deleteBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Остановить стандартную отправку формы
+    console.log('kek');
+    delete_modal.show();
+});
+
+// Находим кнопку удаления в модальном окне
+document.getElementById('delete').addEventListener('click', function() {
+    // Ищем форму, которая уже привязана к кнопке удаления
+    var form = document.getElementById('deleteForm');  // ID формы, которую будем отправлять
+    if (form) {
+        form.submit();  // Отправляем форму
+    }
+});
+
+const edit_btn = document.getElementById('editBtn');
+console.log(edit_btn);
 // Обработчик кнопки удаления данных
 document.getElementById('editBtn').addEventListener('click', function(event) {
     event.preventDefault(); // Остановить стандартную отправку формы
